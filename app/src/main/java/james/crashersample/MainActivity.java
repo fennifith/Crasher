1e2180a5b7ddb0923beb8069cd3c06e473311b8b
James Fenn <18jafenn90@gmail.com>
2017-04-27 16:34:01 -0400

    added options for StackOverflow and CrashActivity enabling/disabling stuff

--- app/src/main/java/james/crashersample/MainActivity.java
+++ app/src/main/java/james/crashersample/MainActivity.java
@@ -1,8 +1,10 @@
 package james.crashersample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import james.crasher.Crasher;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Crasher crasher;
 
+    private SwitchCompat stackOverflowSwitch;
+    private SwitchCompat crashActivitySwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +23,27 @@ public class MainActivity extends AppCompatActivity {
         crasher = new Crasher(this);
         crasher.setEmail("18jafenn90@gmail.com");
 
+        stackOverflowSwitch = (SwitchCompat) findViewById(R.id.stackOverflow);
+        crashActivitySwitch = (SwitchCompat) findViewById(R.id.crashActivity);
+
         findViewById(R.id.nullPointer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((TextView) null).setText("Hi!");
+            }
+        });
+
+        stackOverflowSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                crasher.setForceStackOverflow(isChecked);
+            }
+        });
+
+        crashActivitySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                crasher.setCrashActivityEnabled(isChecked);
             }
         });
     }
--- crasher/src/main/java/james/crasher/Crasher.java
+++ crasher/src/main/java/james/crasher/Crasher.java
@@ -2,7 +2,10 @@ package james.crasher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -13,6 +16,10 @@ public class Crasher implements Thread.UncaughtExceptionHandler {
 
     private Context context;
 
+    private boolean isStackOverflow;
+    private boolean isForceStackOverflow;
+    private boolean isCrashActivity = true;
+
     private String email;
     private Integer color;
 
@@ -21,32 +28,76 @@ public class Crasher implements Thread.UncaughtExceptionHandler {
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
+    public void setStackoverflowEnabled(boolean isEnabled) {
+        isStackOverflow = isEnabled;
+    }
+
+    public boolean isStackoverflowEnabled() {
+        return BuildConfig.DEBUG && isStackOverflow;
+    }
+
+    public void setForceStackOverflow(boolean isForced) {
+        isForceStackOverflow = isForced;
+    }
+
+    public boolean isForceStackOverflow() {
+        return isForceStackOverflow;
+    }
+
+    public void setCrashActivityEnabled(boolean isEnabled) {
+        isCrashActivity = isEnabled;
+    }
+
+    public boolean isCrashActivityEnabled() {
+        return isCrashActivity;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return email;
     }
 
     public void setColor(@ColorInt int color) {
         this.color = color;
     }
 
+    @ColorInt
+    @Nullable
+    public Integer getColor() {
+        return color;
+    }
+
     @Override
     public void uncaughtException(Thread t, final Throwable e) {
-        StringWriter writer = new StringWriter();
-        e.printStackTrace(new PrintWriter(writer));
+        if ((BuildConfig.DEBUG && isStackOverflow) || isForceStackOverflow) {
+            e.printStackTrace();
+            Log.d("Crasher", "Exception thrown: " + e.getClass().getName() + ". Opening StackOverflow query for \"" + e.getMessage() + "\".");
 
-        Intent intent = new Intent(context, CrashActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(CrashActivity.EXTRA_NAME, e.getClass().getName());
-        intent.putExtra(CrashActivity.EXTRA_MESSAGE, e.getLocalizedMessage());
-        intent.putExtra(CrashActivity.EXTRA_STACK_TRACE, writer.toString());
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://stackoverflow.com/search?q=[java][android]" + e.getMessage()));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } else if (isCrashActivity) {
+            StringWriter writer = new StringWriter();
+            e.printStackTrace(new PrintWriter(writer));
 
-        if (email != null)
-            intent.putExtra(CrashActivity.EXTRA_EMAIL, email);
+            Intent intent = new Intent(context, CrashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(CrashActivity.EXTRA_NAME, e.getClass().getName());
+            intent.putExtra(CrashActivity.EXTRA_MESSAGE, e.getLocalizedMessage());
+            intent.putExtra(CrashActivity.EXTRA_STACK_TRACE, writer.toString());
 
-        if (color != null)
-            intent.putExtra(CrashActivity.EXTRA_COLOR, color);
+            if (email != null)
+                intent.putExtra(CrashActivity.EXTRA_EMAIL, email);
 
-        context.startActivity(intent);
+            if (color != null)
+                intent.putExtra(CrashActivity.EXTRA_COLOR, color);
+
+            context.startActivity(intent);
+        } else e.printStackTrace();
 
         System.exit(1);
     }
